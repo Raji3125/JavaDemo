dda4fa1f29cfcfce5b6bdf0c297bf9a61bbc5ec0
Raji3125 <rajig3125@gmail.com>
2023-02-09 16:04:10 -0500

    test commit

---
+++ src/javalearning/Basics.java
@@ -0,0 +1,13 @@
+package javalearning;
+
+public class Basics {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("Hello");
+  int a=10;
+  int b=20;
+  System.out.println("sum of two numbers is: " +(a+b));
+	}
+
+}
